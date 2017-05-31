2e794b79b696b7036010ec07954ae02b0c6330b8
Alexander Coffin <alexandercoffin@Alexanders-MacBook-Pro.local>
2017-05-31 16:26:31 -0500

    Jquery not working

--- resource/main.js
+++ resource/main.js
@@ -1,34 +1,11 @@
-// $(document).ready(function () {
-//     $('.carousel').slick({
-//         autoplay: true,
-//         autoplaySpeed: 4000,
-//         arrows: false,
-//         fade: true,
-//         pauseOnFocus: false,
-//         pauseOnHover: false
-//     });
-// });
+$(document).ready(function () {
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        fade: true,
+        pauseOnFocus: false,
+        pauseOnHover: false
+    });
+});
 
-function createSlick(){  
-
-	$(".slider").not('.slick-initialized').slick({
-    autoplay: true,
-    dots: true,
-    responsive: [{ 
-        breakpoint: 500,
-        settings: {
-            dots: false,
-            arrows: false,
-            infinite: false,
-            slidesToShow: 2,
-            slidesToScroll: 2
-        } 
-    }]
-});	
-
-}
-
-createSlick();
-
-//Now it will not throw error, even if called multiple times.
-$(window).on( 'load', createSlick );
