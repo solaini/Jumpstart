09b73ba6768eebc8071f114d78e79d5a6fe5310f
Alexander Coffin <alexandercoffin@Alexanders-MacBook-Pro.local>
2017-05-31 16:26:03 -0500

    Still working on jquery

--- resource/main.js
+++ resource/main.js
@@ -1,8 +1,34 @@
-$(document).ready(function() {
-    $('.carousel').slick({
-        autoplay: true,
-        autoplaySpeed: 4000,
-        arrows: false,
-        fade: true
-    });
-});
+// $(document).ready(function () {
+//     $('.carousel').slick({
+//         autoplay: true,
+//         autoplaySpeed: 4000,
+//         arrows: false,
+//         fade: true,
+//         pauseOnFocus: false,
+//         pauseOnHover: false
+//     });
+// });
+
+function createSlick(){  
+
+	$(".slider").not('.slick-initialized').slick({
+    autoplay: true,
+    dots: true,
+    responsive: [{ 
+        breakpoint: 500,
+        settings: {
+            dots: false,
+            arrows: false,
+            infinite: false,
+            slidesToShow: 2,
+            slidesToScroll: 2
+        } 
+    }]
+});	
+
+}
+
+createSlick();
+
+//Now it will not throw error, even if called multiple times.
+$(window).on( 'load', createSlick );
